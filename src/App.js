7f8cfeafb40365b04b72a43f64b58fff930b70d4
joneirikboska <joneirikboska@gmail.com>
2020-11-20 20:12:54 +0100

    Display podcast list as flex.

--- src/App.js
+++ src/App.js
@@ -14,19 +14,21 @@ function App() {
 
   return (
     <div className='App'>
-      <form onSubmit={e => submitHandler(e)}>
+      <form onSubmit={e => submitHandler(e)} className='searchBar'>
         <input
           placeholder='Search podcasts'
           value={searchTerm}
           onChange={e => setSearchTerm(e.target.value)}
         />
       </form>
-      {podcasts.map(podcast => (
-        <div key={podcast.collectionId}>
-          <p>{podcast.collectionName}</p>
-          <img src={podcast.artworkUrl100} alt={podcast.collectionName} />
-        </div>
-      ))}
+      <div className='podcast-list'>
+        {podcasts.map(podcast => (
+          <div key={podcast.collectionId} className='podcast'>
+            <p>{podcast.collectionName}</p>
+            <img src={podcast.artworkUrl100} alt={podcast.collectionName} />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
