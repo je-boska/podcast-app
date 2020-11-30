1aca0079e334b8e81db8274a955dab3447b7e2fa
joneirikboska <joneirikboska@gmail.com>
2020-11-30 14:23:47 +0100

    Add headers to axios request to avoid cors blocking.

--- src/PodcastRequests.js
+++ src/PodcastRequests.js
@@ -4,7 +4,7 @@ export async function searchPodcasts(searchTerm) {
   const searchString = `search?entity=podcast&term=${searchTerm}&limit=10`
   const {
     data: { results },
-  } = await axios.get(`https://itunes.apple.com/${searchString}`)
+  } = await axios.get(`https://itunes.apple.com/${searchString}`, {headers: { "Access-Control-Allow-Origin": "https://compassionate-curie-974e23.netlify.app/" }})
   return results
 }
 
@@ -12,6 +12,6 @@ export async function lookupEpisodes(id) {
   const lookupString = `lookup?id=${id}&media=podcast&entity=podcastEpisode&limit=50`
   const {
     data: { results },
-  } = await axios.get(`https://itunes.apple.com/${lookupString}`)
+  } = await axios.get(`https://itunes.apple.com/${lookupString}`, {headers: { "Access-Control-Allow-Origin": "https://compassionate-curie-974e23.netlify.app/" }})
   return results
 }
